2d26b8139d95d21a12853b367d7fcd7224bc9e62
Ksnovaes <kauanovais84@gmail.com>
2025-04-02 21:53:51 -0300

    Initial commit

---
+++ src/test/java/com/ksnovaes/borajogar/BoraJogarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksnovaes.borajogar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoraJogarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
